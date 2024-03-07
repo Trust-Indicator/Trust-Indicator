4bb000d3815ffc0c0fb5359d4368ca604166acb2
SongHan <giay.harry@gmail.com>
2024-03-07 13:28:58 +1100

    creat database in flask

--- src/Flask/app.py
+++ src/Flask/app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+from werkzeug.security import generate_password_hash
+
+from database import db, create_database, User
+from flask import request, jsonify
+from email_validator import validate_email, EmailNotValidError
+
 
 app = Flask(__name__)
+# Creat SQLite Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MyDatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+# create_database(app)
 
 @app.route('/')
 def index():
---
+++ src/Flask/database.py
@@ -0,0 +1,70 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    UserID = db.Column(db.Integer, primary_key=True)
+    UserName = db.Column(db.String(80), unique=True, nullable=False)
+    Email = db.Column(db.String(120), unique=True, nullable=False)
+    Password = db.Column(db.String(100))
+    LegalName = db.Column(db.String(100))
+    ProfilePhotoNO = db.Column(db.String(100))
+    Is_Admin = db.Column(db.Integer)
+
+
+class Metadata(db.Model):
+    __tablename__ = 'Metadata'
+    ImageID = db.Column(db.Integer, primary_key=True)
+    File_Size = db.Column(db.Integer)
+    File_Type = db.Column(db.Text)
+    MIME_Type = db.Column(db.Text)
+    Create_Date = db.Column(db.Text)
+    Modify_Date = db.Column(db.Text)
+    Color_Space = db.Column(db.Text)
+    Make = db.Column(db.Text)
+    Model = db.Column(db.Text)
+    Lens = db.Column(db.Text)
+    Focal_Length = db.Column(db.REAL)
+    Aperture = db.Column(db.REAL)
+    Exposure = db.Column(db.REAL)
+    ISO = db.Column(db.Integer)
+    Flash = db.Column(db.Text)
+    Altitude = db.Column(db.REAL)
+    Latitude = db.Column(db.REAL)
+    Longitude = db.Column(db.REAL)
+    Software = db.Column(db.Text)
+
+class EFMigrationsHistory(db.Model):
+    __tablename__ = '_EFMigrationsHistory'
+    MigrationId = db.Column(db.Text, primary_key=True)
+    ProductVersion = db.Column(db.Text)
+
+class Favorites(db.Model):
+    __tablename__ = 'Favorites'
+    RecordID = db.Column(db.Integer, primary_key=True)
+    ImageID = db.Column(db.Integer)
+    UserID = db.Column(db.Integer)
+    Rate = db.Column(db.Integer)
+    Is_Favorite = db.Column(db.Integer)
+    Comment = db.Column(db.Text)
+    Create_Date = db.Column(db.Text)
+
+class Images(db.Model):
+    __tablename__ = 'Images'
+    ImageID = db.Column(db.Integer, primary_key=True)
+    UserID = db.Column(db.Integer)
+    ImageTitle = db.Column(db.Text)
+    ImageUrl = db.Column(db.Text)
+    ImageDescription = db.Column(db.Text)
+    UploadDate = db.Column(db.Text)
+    Tag = db.Column(db.Text)
+
+def create_database(app):
+    """Create SQLite database if it doesn't exist."""
+    if not os.path.exists('mydatabase.db'):
+        with app.app_context():
+            # db.init_app(app)
+            db.create_all()
+        print("Database created!")
